acce8c4352a41bc5abe9f66341cbb6173062b358
Shirley0904 <sfgrigsby@gmail.com>
2015-11-10 16:04:00 -0500

    removed lecture code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,26 +1,4 @@
 class Post < ActiveRecord::Base
 
-has_many :votes
-belongs_to :user
-
-validates_presence_of :title, :url
-
-validates_format_of :url, :with => /\Ahttp(|s):\/\//
-
-before_validation :add_http
-
-def add_http
-  unless self.url =~ /\Ahttp(|s):\/\//
-    self.url = "http://" + self.url
-  end
-end
-
-def cast_vote(user, value=1)
-  vote = Vote.new(value: value, :user => user)
-  self.votes << vote
-  vote
-end
-
-end
   has_many :votes
 end
